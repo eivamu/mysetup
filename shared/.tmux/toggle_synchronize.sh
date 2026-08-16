f1ada11c1d4e21aea20686cb9ed12c8fd38dc476
Eivind <eha@eha.no>
2026-08-16 03:44:34 +0200

    New colors

--- shared/.tmux/toggle_synchronize.sh
+++ shared/.tmux/toggle_synchronize.sh
@@ -5,13 +5,13 @@ tmux_status=$(tmux show-window-options)
 if echo "$tmux_status" | grep -q "synchronize-panes on"; then
     tmux set-option synchronize-panes off
     tmux set-option window-style 'fg=colour244,bg=colour236'
-    tmux set-option window-active-style 'fg=colour7,bg=colour234'
+    tmux set-option window-active-style 'fg=colour7,bg=colour0'
     tmux set-option pane-border-style 'fg=colour244,bg=colour236'
-    tmux set-option pane-active-border-style 'fg=colour196,bg=colour88'
+    #tmux set-option pane-active-border-style 'fg=colour196,bg=colour88'
 else
     tmux set-option synchronize-panes on
-    tmux set-option window-style 'fg=colour227,bg=colour88'
-    tmux set-option window-active-style 'fg=colour231,bg=colour52'
-    tmux set-option pane-border-style 'fg=colour196,bg=colour88'
-    tmux set-option pane-active-border-style 'fg=colour227,bg=colour196'
+    tmux set-option window-style 'fg=colour7,bg=colour0'
+    tmux set-option window-active-style 'fg=colour255,bg=#2f0000'
+    tmux set-option pane-border-style 'fg=colour244,bg=colour0'
+    #tmux set-option pane-active-border-style 'fg=colour21,bg=colour17'
 fi
